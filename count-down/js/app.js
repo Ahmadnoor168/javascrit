13917855cd076239f0a5b8a75190372f1c551e09
Ahmandnoor168 <ahmadnoor9192@gmail.com>
2023-10-20 11:51:59 +0500

    first commit

--- count-down/js/app.js
+++ count-down/js/app.js
@@ -1,37 +1,25 @@
-const endDate = "27 July 2022 08:20:00 PM"
-
-document.getElementById("end-date").innerText = endDate;
-const inputs = document.querySelectorAll("input")
-    // const clock = () => {
-
-// }
+const endDate = 'Tus Dec 07 2023 07:50:31 GMT+0500'
+// document.getElementById('end-date').innerText = endDate
+const inputs = document.querySelectorAll('input')
 
 function clock() {
-    const end = new Date(endDate)
-    const now = new Date()
-    const diff = (end - now) / 1000;
+  const end = new Date(endDate)
+  const now = new Date()
+  console.log(now)
+  const diff = (end - now) / 1000
 
-    if (diff < 0) return;
-
-    // convert into days;
-    inputs[0].value = Math.floor(diff / 3600 / 24);
-    inputs[1].value = Math.floor(diff / 3600) % 24;
-    inputs[2].value = Math.floor(diff / 60) % 60;
-    inputs[3].value = Math.floor(diff) % 60;
+  if (diff < 0) return
+  console.log(Math.floor(diff / 60) % 60)
+  // convert into days;
+  inputs[0].value = Math.floor(diff / 3600 / 24)
+  inputs[1].value = Math.floor(diff / 3600) % 24
+  inputs[2].value = Math.floor(diff / 60) % 60
+  inputs[3].value = Math.floor(diff) % 60
 }
 
 // initial call
 clock()
 
-/**
- *  1 day = 24 hrs
- *  1 hr = 60 mins
- *  60 min = 3600 sec
- */
-
-setInterval(
-    () => {
-        clock()
-    },
-    1000
-)
+setInterval(() => {
+  clock()
+}, 1000)
